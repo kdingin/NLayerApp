461f3beae66e2d23ce7166fdc7c3e4c3df636ce2
fatih Küçükdingin <68949521+kdingin@users.noreply.github.com>
2023-09-20 22:12:35 +0300

    MVC

---
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.Services;
+
+namespace NLayer.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _service;
+        public ProductsController(IProductService service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _service.GetProductsWithCategory());
+        }
+    }
+}
